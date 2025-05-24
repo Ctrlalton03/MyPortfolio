1f153df1777e19b1b8522e461b8b2d0501134190
Ctrlalton03 <altonsimpson22@gmail.com>
2025-05-24 17:19:40 -0400

    homepage intro

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -4,16 +4,18 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="home-text-container">
-        <div className="home-section-text">
-          <h2 className="Intro-left-side">
-            Hi I'm
-          </h2>
           <div className="name-wrapper">
+            <div className="Left-side-container">
+              <h2 className="Intro-left-side">Hi I'm</h2>
+              <div className="Left-side-Line"></div>
+
+            </div>
             <h1 className="name">Alton</h1>
             <h2 className="job-title">Frontend Web <br /> Developer</h2>
           </div>
-        </div>
-        <button className='continue-button'><a href='/'>Continue</a></button>
+          <div className="Button-Container">
+          <button className='continue-button'><a href='/'>Continue</a></button>
+          </div>
       </div>
     </div>
   )
