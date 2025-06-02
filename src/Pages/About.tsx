d16d89f5a45e958784015f7fba1a7b36429d10ee
Ctrlalton03 <altonsimpson22@gmail.com>
2025-06-02 12:56:18 -0400

    started the about page

--- src/Pages/About.tsx
+++ src/Pages/About.tsx
@@ -1,8 +1,37 @@
+import '../CSS/About.css'
+
+
+
+
+
+
+
 const About = () => {
   return (
-    <div>
-      <h1>About</h1>
+    <>
+
+    <div className='About-Container'>
+          
+      <h1 className='About-Header'>About Alton</h1>
+      <div className='Header-underline'></div>
+      <div className='About-Image-Container'>
+        <p>Self Image here</p>
+      </div> 
+      <div className='About-header-Container'>
+        <h2 className='About-Header-Text'>Hi, I'm Alton</h2>
+      </div>
+      <div className='About-Text'>
+        <p>
+          A front-end developer with a passion for turning ideas into clean, interactive web experiences. I specialize in building responsive 
+          websites using HTML, CSS, JavaScript, and React, and I enjoy creating designs that feel just as good as they look. Whether I’m working 
+          on a simple landing page or a dynamic web app, I focus on clean code, smooth user flows, and thoughtful design. When I’m not coding, 
+          I enjoy learning new tech, collaborating with others, and staying inspired by what’s possible on the web.
+        </p>
+      </div>
     </div>
+
+
+    </>
   )
 }
 
