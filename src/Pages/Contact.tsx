fdc92df8bf0085a22c260c89d90fd90d67b588a4
Alton Simpson <altonsimpson22@gmail.com>
2025-06-22 22:06:36 -0400

    touching up on contact page

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,9 +1,16 @@
-
+import '../CSS/Contact.css'
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='ContactContainer'>
+      <div className='ContactHeader'>
+        <h1 className='ContactTitle'>Get in Touch</h1>
+        <p className='ContactDescription'>
+          I would love to hear from you! Whether you have a question, want <br /> 
+          to collaborate on a project, or just want to say hello, feel free to reach 
+          out.
+        </p>
+      </div>
     </div>
   )
 }
